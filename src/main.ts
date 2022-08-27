a28ae0d6b0f4aa192604c1cb8c1e40fd55a92f89
김동하 <supercurve2256@gmail.com>
2022-08-27 22:32:11 +0900

    timeout global set

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from './common/interceptors/success.interceptor';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new SuccessInterceptor(), new TimeoutInterceptor());
 
   app.enableCors({
     origin: true,
